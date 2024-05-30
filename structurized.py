b7a0d5d5e6db08b6bb697790db82d8efab04a8fb
BekaBitarashvili <bekabitarashvili@gmail.com>
2024-05-30 10:56:58 +0400

    Modified

--- structurized.py
+++ structurized.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec, wait
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By

ba2f276981412e4cac5ecf831153c91995bef288
anabanadvali <bekabitarashvili@gmail.com>
2024-08-27 11:19:35 +0400

    Modified

--- structurized.py
+++ structurized.py
@@ -190,7 +190,7 @@ class TestWebsite(unittest.TestCase):
         self.driver.find_element(By.XPATH,
                                  "/html/body/ufe-root/div/div/as-customers/ufe-body-layout/div/main/div/as-customer"
                                  "-create/kendo-tabstrip/div[2]/as-customer-general/div/div/div[3]/form/div[1]/div["
-                                 "1]/div[2]/div[4]/input").send_keys("აგარა")
+                                 "1]/div[2]/div[4]/input").send_keys("ირანი")
         # TRIPLE CLICK
         elemento_to_triple_click = self.driver.find_element(By.XPATH,
                                                             "/html/body/ufe-root/div/div/as-customers/ufe-body-layout"
@@ -317,7 +317,7 @@ class TestWebsite(unittest.TestCase):
         self.driver.find_element(By.XPATH,
                                  "/html/body/ufe-root/div/div/as-customers/ufe-body-layout/div/main/div/as-customer"
                                  "-create/kendo-tabstrip/div[2]/as-customer-general/div/div/div[3]/form/div[3]/div/div["
-                                 "2]/div/div/div[2]/input").send_keys("ქართლი")
+                                 "2]/div/div/div[2]/input").send_keys("ირანი")
         self.driver.find_element(By.XPATH,
                                  "/html/body/ufe-root/div/div/as-customers/ufe-body-layout/div/main/div/as-customer"
                                  "-create/kendo-tabstrip/div[2]/as-customer-general/div/div/div[3]/form/div[3]/div/div["
@@ -326,7 +326,7 @@ class TestWebsite(unittest.TestCase):
         self.driver.find_element(By.XPATH,
                                  "/html/body/ufe-root/div/div/as-customers/ufe-body-layout/div/main/div/as-customer"
                                  "-create/kendo-tabstrip/div[2]/as-customer-general/div/div/div[3]/form/div[3]/div/div["
-                                 "4]/div/div/div[2]/input").send_keys("ქართლი")
+                                 "4]/div/div/div[2]/input").send_keys("ირანი")
         self.driver.find_element(By.XPATH,
                                  "/html/body/ufe-root/div/div/as-customers/ufe-body-layout/div/main/div/as-customer"
                                  "-create/kendo-tabstrip/div[2]/as-customer-general/div/div/div[3]/form/div[3]/div/div["
